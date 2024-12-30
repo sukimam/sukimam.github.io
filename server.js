5ae430b2d9b052d34759af2658e8b2a01f70351c
Sukairaj <sukhimam00@gmail.com>
2024-12-30 20:49:46 +0100

    Add proxy server for fetching manifest.json

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/proxy/manifest', async (req, res) => {
+    const fetch = await import('node-fetch');
+    const response = await fetch.default('https://github.com/sukimam.github.io/images/manifest.json');
+    const data = await response.text();
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+});
+
+app.listen(3000, () => console.log('Proxy server running on port 3000'));
